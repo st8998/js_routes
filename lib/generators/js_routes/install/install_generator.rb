24889c029d5a75c1a1a2ce37a8431130e0474888
Anatoly Lapshin <technologymasta@gmail.com>
2011-08-04 14:35:00 +0400

    fixes and version bump

--- lib/generators/js_routes/install/install_generator.rb
+++ lib/generators/js_routes/install/install_generator.rb
@@ -5,7 +5,7 @@ module JsRoutes
     class InstallGenerator < ::Rails::Generators::Base
 
       desc "This generator install JsRoutes pathBuilder.js which contains utils for building routes on client side"
-      source_root File.expand_path('../../../assets/javascripts', __FILE__)
+      source_root File.expand_path('../../../../assets/javascripts', __FILE__)
 
       def copy_pathbuilder
         say_status("copying", "jquery.pathBuilder.js (#{JsRoutes::VERSION})", :green)
